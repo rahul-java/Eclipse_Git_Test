2385f0956397407dd3d91c0b26fd5aebed3db130
Rahul Kumar Pandey <rp201291@gmail.com>
2024-05-14 21:34:43 +0530

    TL modified and added App.java file

--- 07_MavenProject_GitPracticeProject/src/main/java/com/mea/App.java
+++ 07_MavenProject_GitPracticeProject/src/main/java/com/mea/App.java
@@ -6,8 +6,25 @@ package com.mea;
  */
 public class App 
 {
+	public int add(int x,int y) {
+		return x+y;
+	}
+	
+	public int sub(int x,int y) {
+		return x-y;
+	}
+	
+	public int mul(int x,int y) {
+		return x*y;
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+    	System.out.println("App.main()");
+    	App app = new App();
+        System.out.println("Sum is ::: "+app.add(10, 20));
+        System.out.println("Subtraction is :: "+app.sub(20, 20));
+        System.out.println("Multiplication is :"+app.mul(10, 20));
     }
 }
