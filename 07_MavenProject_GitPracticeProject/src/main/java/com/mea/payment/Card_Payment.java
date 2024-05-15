a26735964f4a0a7825416becf5eaa4c1c9d38df7
Rahul Kumar Pandey <rp201291.java@gmail.com>
2024-05-15 07:55:27 +0530

    TL ::: CardPayment is added.

---
+++ 07_MavenProject_GitPracticeProject/src/main/java/com/mea/payment/Card_Payment.java
@@ -0,0 +1,9 @@
+package com.mea.payment;
+
+public class Card_Payment {
+
+	public String doPayment(long cardNo,double amount) {
+		System.out.println("Card_Payment.doPayment()");
+		return "Card Payment Done Successfully";
+	}
+}
